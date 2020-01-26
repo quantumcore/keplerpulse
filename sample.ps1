cb4f5e8bb344d72a59556587f87e07fa7785d99f
Fahad <fahad.mustafa0012@gmail.com>
2020-01-26 18:34:09 +0500

    Update sample.ps1

--- sample.ps1
+++ sample.ps1
@@ -1,3 +1,4 @@
 # Fix and update this code.
 # Run as -windowstyle hidden
-(new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/quantumcore/supercharge/master/README.md','C:\Users\%USERNAME%\Desktop\README.md')
+$b = aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3F1YW50dW1jb3JlL3N1cGVyY2hhcmdlL21hc3Rlci9SRUFETUUubWQ= $a =[System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($b))
+$a (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/quantumcore/supercharge/master/README.md','C:\Users\%USERNAME%\Desktop\README.md')
